52582067e7913cb047d57b6abb2a89e0b770c8d2
antonyedo <ant.fedorich@gmail.com>
2026-09-22 13:41:52 +0200

    add libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -36,14 +37,18 @@ android {
 }
 
 dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    implementation(libs.bundles.compose)
+
+    implementation(platform(libs.koin.bom))
+    implementation(libs.bundles.koin)
+
+    implementation(libs.bundles.retrofit)
+    implementation(libs.bundles.moshi)
+    implementation(libs.bundles.kotlinx.serialization)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
