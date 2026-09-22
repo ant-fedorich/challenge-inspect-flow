447f13b7f0f7a7df3f389ce100f965d3f7691e0e
antonyedo <ant.fedorich@gmail.com>
2026-09-22 18:53:50 +0200

    add navigation lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.bundles.compose)
+    implementation(libs.androidx.navigation.compose)
 
     implementation(platform(libs.koin.bom))
     implementation(libs.bundles.koin)
