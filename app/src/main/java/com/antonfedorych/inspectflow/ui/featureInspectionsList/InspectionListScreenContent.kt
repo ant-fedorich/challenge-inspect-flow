db138ba926fcd0cc5ce99146f6042925c68f2de1
antonyedo <ant.fedorich@gmail.com>
2026-09-22 21:01:56 +0200

    add showing list with depth

--- app/src/main/java/com/antonfedorych/inspectflow/ui/featureInspectionsList/InspectionListScreenContent.kt
+++ app/src/main/java/com/antonfedorych/inspectflow/ui/featureInspectionsList/InspectionListScreenContent.kt
@@ -1,13 +1,28 @@
 package com.antonfedorych.inspectflow.ui.featureInspectionsList
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -16,6 +31,7 @@ fun InspectionListScreenContent(
     state: InspectionListState = InspectionListState(),
     onEvent: (InspectionListEvent) -> Unit = {}
 ) {
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -23,9 +39,35 @@ fun InspectionListScreenContent(
             .padding(horizontal = 20.dp)
             .padding(vertical = 24.dp)
     ) {
-        Text(
-            text = state.items.toString()
-        )
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(state.items) { item ->
+                Box(
+                    Modifier.padding(start = 24.dp * item.depth)
+                ) {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .background(Color.Gray.copy(0.4f), shape = ShapeDefaults.Medium)
+                            .padding(24.dp)
+
+                    ) {
+                        Text(
+                            text = "#" + item.id.toString()
+                        )
+                        Spacer(Modifier.width(8.dp))
+                        Text(
+                            text = item.type.name
+                        )
+                        Spacer(Modifier.width(8.dp))
+                        Text(
+                            text = item.depth.toString()
+                        )
+                    }
+                }
+            }
+        }
     }
 }
 
--- app/src/main/java/com/antonfedorych/inspectflow/ui/featureInspectionsList/mapper/InspectionListRowMapper.kt
+++ app/src/main/java/com/antonfedorych/inspectflow/ui/featureInspectionsList/mapper/InspectionListRowMapper.kt
@@ -11,17 +11,21 @@ import com.antonfedorych.inspectflow.domain.model.TextQuestionItem
 import com.antonfedorych.inspectflow.ui.featureInspectionsList.uimodel.ChoiceOptionRow
 import com.antonfedorych.inspectflow.ui.featureInspectionsList.uimodel.InspectionListRow
 
-fun List<Item>.toUIModel(): List<InspectionListRow> {
+fun List<Item>.toUIModel(depth: Int = 0): List<InspectionListRow> {
     val list = mutableListOf<InspectionListRow>()
     forEach { item ->
         if (item is PageItem) {
-            list.add(item.toUIModel())
-            if (item.items.isNotEmpty()) list.addAll(item.items.toUIModel())
+            list.add(item.toUIModel(depth))
+            if (item.items.isNotEmpty()) {
+                list.addAll(item.items.toUIModel(depth + 1))
+            }
         } else if (item is SectionItem) {
-            list.add(item.toUIModel())
-            if (item.items.isNotEmpty()) list.addAll(item.items.toUIModel())
+            list.add(item.toUIModel(depth))
+            if (item.items.isNotEmpty()) {
+                list.addAll(item.items.toUIModel(depth + 1))
+            }
         } else {
-            list.add(item.toUIModel())
+            list.add(item.toUIModel(depth))
         }
     }
 
@@ -36,24 +40,24 @@ fun ResponseSet.toUIModel(): List<ChoiceOptionRow> =
         )
     }
 
-fun Item.toUIModel(): InspectionListRow {
+fun Item.toUIModel(depth: Int = 0): InspectionListRow {
     val item = when (this) {
         is PageItem -> InspectionListRow(
             id = this.id,
             type = this.type,
-            depth = 0,
+            depth = depth,
             title = this.title,
         )
         is SectionItem -> InspectionListRow(
             id = this.id,
             type = this.type,
-            depth = 0,
+            depth = depth,
             title = this.title,
         )
         is ChoiceQuestionItem -> InspectionListRow(
             id = this.id,
             type = this.type,
-            depth = 0,
+            depth = depth,
             content = this.content,
             multipleSelection = this.responseSet.multipleSelection,
             choiceOptions = this.responseSet.toUIModel(),
@@ -61,14 +65,14 @@ fun Item.toUIModel(): InspectionListRow {
         is ImageQuestionItem -> InspectionListRow(
             id = this.id,
             type = this.type,
-            depth = 0,
+            depth = depth,
             title = this.title,
             imageSrc = this.src,
         )
         is TextQuestionItem -> InspectionListRow(
             id = this.id,
             type = this.type,
-            depth = 0,
+            depth = depth,
             content = this.content
         )
     }
